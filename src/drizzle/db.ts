778240943804e564eee656c332e9afa853eb1b0c
xinzhang <xin_zhang_2000@yahoo.com>
2025-04-13 00:36:21 +1000

    add drizzle ws for transaction (in progress

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -1,21 +1,24 @@
 import { env } from "@/data/env/server"
 import { drizzle } from "drizzle-orm/node-postgres"
-import { drizzle as drizzleNeon } from "drizzle-orm/neon-http";
-import { neon } from "@neondatabase/serverless";
+import { drizzle as drizzleNeon } from "drizzle-orm/neon-serverless";
+import { createClient, neonConfig } from "@neondatabase/serverless";
+import ws from 'ws';
 
 import * as schema from "./schema"
 
 import { PostgresJsDatabase } from "drizzle-orm/postgres-js"
 
 let db: PostgresJsDatabase<typeof schema>
-
-console.info("env", env)
-
 if (!env.LOCALDEV) {
-  console.log("Using neon database", process.env.VERCEL)
-  db = drizzleNeon(neon(env.DATABASE_URL), { schema })
 
+  console.log("Using neon database", process.env.VERCEL)
+  global.WebSocket = ws as any;
+  neonConfig.webSocketConstructor = ws;
+  // const sql = neon(process.env.DATABASE_URL!);
+  const client = createClient({ connectionString: env.DATABASE_URL })
+  db = drizzleNeon(client, { schema });
 } else {
+
   console.log("Using local database")
 
   db = drizzle({
