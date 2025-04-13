5009c54c9b9f25b2d40e17c7ccb38dd578e30613
xinzhang <xin_zhang_2000@yahoo.com>
2025-04-13 23:43:52 +1000

    add github workflow

---
+++ roles.sh
@@ -0,0 +1,25 @@
+# create service account
+gcloud iam service-accounts create github-actions-deployer \
+  --description="Deploys from GitHub Actions" \
+  --display-name="GitHub Actions Deployer"
+
+
+# grant roles
+gcloud projects add-iam-policy-binding xz-share \
+  --member="serviceAccount:github-actions-deployer@xz-share.iam.gserviceaccount.com" \
+  --role="roles/run.admin"
+
+gcloud projects add-iam-policy-binding xz-share \
+  --member="serviceAccount:github-actions-deployer@xz-share.iam.gserviceaccount.com" \
+  --role="roles/iam.serviceAccountUser"
+
+gcloud projects add-iam-policy-binding xz-share \
+  --member="serviceAccount:github-actions-deployer@xz-share.iam.gserviceaccount.com" \
+  --role="roles/storage.admin"
+
+gcloud iam service-accounts keys create key.json \
+  --iam-account=github-actions-deployer@xz-share.iam.gserviceaccount.com
+
+# create json key
+gcloud iam service-accounts keys create key.json \
+  --iam-account=github-actions-deployer@xz-share.iam.gserviceaccount.com
